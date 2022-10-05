e904268eae622aa71cae2c03235530051b2f997c
Donggun.Chung <chungdk1117@gmail.com>
2022-10-05 21:30:11 +0900

    :hammer: 2022.10.05.Wed [Composition, Inheritence]

---
+++ practice_01/src/chapter_13/ProfileCard.jsx
@@ -0,0 +1,12 @@
+import Card from './Card';
+
+function ProfileCard(props) {
+    return (
+        <Card title="Donggun Chung" backgroundColor="#4ea04e">
+            <p>안녕하세요.</p>
+            <p>리액트를 사용한 개발!</p>
+        </Card>
+    );
+}
+
+export default ProfileCard;
